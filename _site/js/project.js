5a6d3c6841c20898fbebdf876dbc0e026fedb734
Ganesh Iyer <ganesh.iyer.viswanathan@gmail.com>
2017-02-20 01:47:09 -0800

    Minor fixes

---
+++ _site/js/project.js
@@ -0,0 +1,54 @@
+jQuery(document).ready(function(){
+  var viewPortWidth = jQuery(window).width();
+  var viewPortHeight = jQuery(window).height();
+  var totalDocHeight = jQuery('#footer').offset().top;
+  var numSection = $('.section').length;
+
+  // Dynamically generate anchor list
+  var sectionArray = new Array(numSection);
+  sectionArray.fill("page");
+  sectionArray[0] = "top";
+  // var lastPos = jQuery("#last").offset().top;
+  $('#fullpage').fullpage({
+    css3: true,
+    scrollBar: false,
+    scrollingSpeed: 700,
+    verticalCentered: false,
+    responsiveWidth: 768, //was 768, 980, 840
+    easing: 'easeInOutCubic',
+    touchSensitivity: 15,
+    fixedElements: '#height-bar, #go-to-top',
+    anchors: sectionArray,
+    onLeave: function(index, nextIndex, direction){
+            var leavingSection = $(this);
+
+            //after leaving section 2
+            if(index == 1 && direction =='down'){
+                /* make go-to-top appear fully */
+                $("#go-to-top").fadeToggle( "slow", "linear" );
+            }
+
+            if(nextIndex == 1 && direction == 'up'){
+              $("#go-to-top").fadeToggle( "slow", "linear" );
+            }
+        },
+    afterLoad: function(anchorLink, index){
+            var loadedSection = $(this);
+            if(anchorLink == "top"){
+              $("#go-to-top").css("display", "none");
+            }
+        }
+    });
+
+  jQuery(window).scroll(function(){
+    var scrollPos = jQuery(window).scrollTop();
+    var heightBarWidth = scrollPos*100/(totalDocHeight + 2*viewPortHeight);
+    if(heightBarWidth >= 100){
+      $('#height-bar').css("width", 100 + "%");
+    }
+    else{
+    $('#height-bar').css("width", heightBarWidth + "%");
+    }
+  });
+
+});
